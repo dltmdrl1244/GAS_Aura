54515d82b60890b9cb0391af3988f6ef80a7f7ea
dltmdrl1244 <dltmdrl1244@naver.com>
2024-08-16 00:30:58 +0900

    Character 블루프린트 셋업 CharacterBase -> Character, Enemy 클래스 및 블루프린트 생성

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -7,6 +7,10 @@ AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
 }
 
 void AAuraCharacterBase::BeginPlay()
--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -16,5 +16,8 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category = Combat)
+	TObjectPtr<USkeletalMeshComponent> Weapon;
 	
 };
