e41dec430493cdea6f9a079f6bdc30d213a732fa
dltmdrl1244 <dltmdrl1244@naver.com>
2024-08-20 16:05:51 +0900

    Feat: Gameplay Abilities 플러그인 추가 PlayerState, CharacterBase 클래스에 AbilitySystemInterface 및 ASC, AS 포인터 생성

---
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AbilitySystemInterface.h"
+#include "AuraPlayerState.generated.h"
+
+class UAbilitySystemComponent;
+class UAttributeSet;
+
+UCLASS()
+class AURA_API AAuraPlayerState : public APlayerState, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	AAuraPlayerState();
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	UAttributeSet* GetAttributeSet() const {return AttributeSet;}
+
+protected:
+	UPROPERTY()
+	TObjectPtr<class UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY()
+	TObjectPtr<class UAttributeSet> AttributeSet;
+};
